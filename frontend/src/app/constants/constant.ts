1145b1299b615a1af9ad0be94dc4d92d94be8caf
Parthib Datta <parthibdutta02@gmail.com>
2024-07-05 02:22:13 +0530

    Sync
    Signed-off-by: Parthib Datta <parthibdutta02@gmail.com>

--- frontend/src/app/constants/constant.ts
+++ frontend/src/app/constants/constant.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL = "https://bridgebackend-6170enr8h-ptdattas-projects.vercel.app/api/v1";
+export const SERVER_URL = "https://bridgebackend-ptdattas-projects.vercel.app/api/v1";
